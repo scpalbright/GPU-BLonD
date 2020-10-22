bedfccc17530dc30fbc755fb910584636f86315d
Konstantinos Iliakis <konstantinos.iliakis@cern.ch>
2020-10-22 11:39:46 +0300

    using glob to process multiple dirs

--- scripts/other/delete_broken_dirs.py
+++ scripts/other/delete_broken_dirs.py
@@ -2,6 +2,7 @@
 import os
 import argparse
 import shutil as sh
+import glob
 
 this_directory = os.path.dirname(os.path.realpath(__file__)) + '/'
 
@@ -24,26 +25,29 @@ parser.add_argument('-w', '--word', default='Done',
 if __name__ == '__main__':
     args = parser.parse_args()
     to_remove = []
-    for dirs, subdirs, files in os.walk(args.indir):
-        if ('log' not in subdirs) or ('report' not in subdirs):
-            continue
-        error_str = None
-        if 'output.txt' not in files:
-            error_str = 'Dir: {} -- Missing output.txt'.format(dirs)
-        elif ('error.txt' in files) and ('DUE TO TIME LIMIT' in open(dirs + '/error.txt').read()):
-            error_str = 'Dir: {} -- Job aborted due to time limit.'.format(dirs)
-        elif args.word not in open(dirs + '/output.txt').read():
-            error_str = 'Dir: {} -- {} not in output.txt'.format(
-                dirs, args.word)
-        if error_str:
-            print(error_str)
-            if args.action == 'rm':
-                if args.dontask:
-                    ans = 'y'
-                else:
-                    ans = input('Delete? (Y/N) << ').lower()
-                if ans in ['yes', 'y']:
-                    to_remove.append(dirs)
+    indirs = glob.glob(args.indir)
+    for indir in indirs:
+        print('\n------ Processing {} -------'.format(indir))
+        for dirs, subdirs, files in os.walk(args.indir):
+            if ('log' not in subdirs) or ('report' not in subdirs):
+                continue
+            error_str = None
+            if 'output.txt' not in files:
+                error_str = 'Dir: {} -- Missing output.txt'.format(dirs)
+            elif ('error.txt' in files) and ('DUE TO TIME LIMIT' in open(dirs + '/error.txt').read()):
+                error_str = 'Dir: {} -- Job aborted due to time limit.'.format(dirs)
+            elif args.word not in open(dirs + '/output.txt').read():
+                error_str = 'Dir: {} -- {} not in output.txt'.format(
+                    dirs, args.word)
+            if error_str:
+                print(error_str)
+                if args.action == 'rm':
+                    if args.dontask:
+                        ans = 'y'
+                    else:
+                        ans = input('Delete? (Y/N) << ').lower()
+                    if ans in ['yes', 'y']:
+                        to_remove.append(dirs)
 
-    for directory in to_remove:
-        sh.rmtree(directory)
+        for directory in to_remove:
+            sh.rmtree(directory)
